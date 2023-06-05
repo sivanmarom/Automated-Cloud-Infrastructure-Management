8d29a22b4d4eda4559420c8f0884236f656d403f
sivan marom <sivmamrom@gmail.com>
2023-06-05 19:28:43 +0300

    install terraform on agents

--- modules/agents-module/main.tf
+++ modules/agents-module/main.tf
@@ -18,8 +18,10 @@ resource "aws_instance" "ec2-agents" {
     provisioner "remote-exec" {
     inline = [
       "sudo apt update -y",
-      "sudo apt -y install docker.io",
       "sudo apt -y install default-jre",
+      " wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg",
+      "echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com \$(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/hashicorp.list",
+      "sudo apt update && sudo apt install terraform"
     ]
   }
 }
