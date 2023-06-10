c0f6dcfe5a50755ad66b4b0027b6c4d0228a29c6
sivan marom <sivmamrom@gmail.com>
2023-06-10 17:49:08 +0300

    co

--- modules/load-balancer-module/variables.tf
+++ modules/load-balancer-module/variables.tf
@@ -20,19 +20,22 @@ variable "target_group_protocol" {
 
 variable "vpc_id" {
   type    = string
-  default = "vpc-05beb2b3e64cc2c75"  
+  default = "vpc-05beb2b3e64cc2c75"
 }
 
 variable "security_group" {
-  type = string
+  type    = string
   default = "sg-03915679baa198009"
 }
 
 variable "subnets" {
-  type = list(string)
-  default = [ "subnet-0950fcd396ff7984e","subnet-0816347f6f77da266","subnet-0c04c4c76e62a996e","subnet-0284211308afcc97d","subnet-0e287e27d1d7ca185","subnet-0a8f7034ba7a7730c"]
+  type    = list(string)
+  default = ["subnet-0950fcd396ff7984e", "subnet-0816347f6f77da266", "subnet-0c04c4c76e62a996e", "subnet-0284211308afcc97d", "subnet-0e287e27d1d7ca185", "subnet-0a8f7034ba7a7730c"]
 }
 
-
+variable "instance_ids" {
+  type    = list(string)
+  default = []
+}
 
 
