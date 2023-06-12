830ab3c14ce4b350ea6d3b8040a70097df6cfc94
sivan marom <sivmamrom@gmail.com>
2023-06-12 17:47:20 +0300

    commit

--- modules/load-balancer-module/main.tf
+++ modules/load-balancer-module/main.tf
@@ -11,6 +11,17 @@ resource "aws_lb" "load_balancer" {
   count              = 1
 }
 
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.load_balancer[0].arn
+  port              = var.target_group_port
+  protocol          = var.target_group_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
 resource "aws_lb_target_group" "target_group" {
   name     = var.target_group_name
   port     = var.target_group_port
@@ -18,21 +29,8 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.load_balancer[count.index].arn
-  port              = var.target_group_port
-  protocol          = var.target_group_protocol
-
-  default_action {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    type             = "forward"
-  }
-
-  count = 1
-}
-
-resource "aws_lb_target_group_attachment" "target_group_attachment" {
-  count            = 2
+resource "aws_lb_target_group_attachment" "attachments" {
+  count            = length(var.instances_public_ips)
   target_group_arn = aws_lb_target_group.target_group.arn
   target_id        = var.instances_public_ips[count.index]
   port             = var.target_group_port
