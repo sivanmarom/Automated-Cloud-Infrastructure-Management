2307c1ca4370676efb52ec9c2a0095b30d43b52a
sivan marom <sivmamrom@gmail.com>
2023-06-12 11:42:58 +0300

    mi

--- modules/ec2-module/main.tf
+++ modules/ec2-module/main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-
 }
 
 resource "aws_instance" "jenkins_master" {
@@ -8,27 +7,21 @@ resource "aws_instance" "jenkins_master" {
   instance_type          = var.instnace_type
   key_name               = "project_3"
   vpc_security_group_ids = [var.security_group]
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("${path.module}/project_3.pem")
-    timeout     = "10m"
-  }
+
+  user_data = <<-EOT
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt -y install docker.io
+    sudo apt -y install openjdk-8-jre
+    sudo docker run --name jenkins_master -p 8080:8080 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+    EOT
 
   tags = {
     Name = var.jenkisn_master_instance
   }
+
   lifecycle {
     create_before_destroy = true
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt -y install docker.io",
-      "sudo apt -y install openjdk-8-jre",
-      "sudo docker run --name jenkins_master -p 8080:8080 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts",
-    ]
   }
 }
 
@@ -37,26 +30,20 @@ resource "aws_instance" "testing" {
   instance_type          = var.instnace_type
   key_name               = "project_3"
   vpc_security_group_ids = [var.security_group]
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("${path.module}/project_3.pem")
-    timeout     = "10m"
-  }
+
+  user_data = <<-EOT
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt -y install docker.io
+    sudo apt -y install openjdk-8-jre
+    EOT
 
   tags = {
     Name = var.testing_instance
   }
+
   lifecycle {
     create_before_destroy = true
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt -y install docker.io",
-      "sudo apt -y install openjdk-8-jre",
-    ]
   }
 }
 
@@ -66,29 +53,22 @@ resource "aws_instance" "production" {
   instance_type          = var.instnace_type
   key_name               = "project_3"
   vpc_security_group_ids = [var.security_group]
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("${path.module}/project_3.pem")
-    timeout     = "10m"
-  }
+
+  user_data = <<-EOT
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt -y install docker.io
+    sudo apt -y install openjdk-8-jre
+    EOT
 
   tags = {
     Name = var.production_instances[count.index]
   }
+
   lifecycle {
     create_before_destroy = true
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt -y install docker.io",
-      "sudo apt -y install openjdk-8-jre",
-    ]
-  }
 }
-
 
 output "instance_ids" {
   value = join(",", aws_instance.production[*].id)
